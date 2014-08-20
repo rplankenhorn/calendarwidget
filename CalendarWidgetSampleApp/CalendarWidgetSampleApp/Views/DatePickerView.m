54ec3f7802cc3167234324ba3c1b99dc79b55613
Robbie Plankenhorn <rplankenhorn@gmail.com>
2014-08-19 20:55:54 -0500

    Made all magic numbers constants.

--- CalendarWidgetSampleApp/CalendarWidgetSampleApp/Views/DatePickerView.m
+++ CalendarWidgetSampleApp/CalendarWidgetSampleApp/Views/DatePickerView.m
@@ -17,7 +17,28 @@ static NSString * const kDatePickerCollectionViewCellIdentifier = @"DatePickerCo
 static NSString * const kDatePickerCollectionViewCellXib        = @"DatePickerCollectionViewCell";
 static NSUInteger const kDaysInWeek                             = 7;
 
+static CGFloat const kCollectionViewHeightBuffer                = 7.0f;
+static CGFloat const kViewWidth                                 = 316.0f;
+static CGFloat const kHeaderViewHeight                          = 101.0f;
+static CGSize const kDayLabelSize                               = {15.0f, 21.0f};
+static CGSize const kChevronSize                                = {26.0f, 27.0f};
+static CGFloat const kChevronSpacing                            = 10.0f;
+static CGFloat const kMonthTitleLabelHeight                     = 21.0f;
+
+static UIEdgeInsets const kSectionInset                         = {1.0f, 1.0f, 1.0f, 1.0f};
+static CGSize const kCollectionViewCellSize                     = {44.0f, 44.0f};
+static CGFloat const kMinimumInteritemSpacing                   = 1.0f;
+static CGFloat const kMinimumLineSpacing                        = 1.0f;
+
+static NSString * const kDefaultFont                            = @"HelveticaNeue";
+static NSString * const kDefaultFontBold                        = @"HelveticaNeue-Bold";
+
+static NSString * const kLeftChevronImageName                   = @"left_chevron";
+static NSString * const kRightChevronImageName                  = @"right_chevron";
+
 @interface DatePickerView () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+// UI Elements
 @property (strong, nonatomic) UIView *headerView;
 @property (strong, nonatomic) NSMutableArray *dayLabels;
 @property (strong, nonatomic) UIButton *leftChevronButton;
@@ -25,12 +46,14 @@ static NSUInteger const kDaysInWeek                             = 7;
 @property (strong, nonatomic) UIButton *rightChevronButton;
 @property (strong, nonatomic) UICollectionView *collectionView;
 @property (strong, nonatomic) CalendarFlowLayout *calendarFlowLayout;
+
+// Local properties
 @property (strong, nonatomic) NSCalendar *calendar;
 @property (assign, nonatomic, readonly) NSInteger offset;
 @property (assign, nonatomic, readonly) NSInteger endPadding;
 @property (strong, nonatomic) NSIndexPath *selectedIndex;
-
 @property (strong, nonatomic, readonly) NSArray *daysOfWeekPrefixes;
+
 @end
 
 @implementation DatePickerView
@@ -51,7 +74,7 @@ static NSUInteger const kDaysInWeek                             = 7;
         
         for (int i=0; i<kDaysInWeek; i++) {
             UILabel *dayLabel = [[UILabel alloc] init];
-            dayLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:17.0f];
+            dayLabel.font = [UIFont fontWithName:kDefaultFontBold size:17.0f];
             dayLabel.textColor = [UIColor colorWithRed:201.0f/255.0f green:201.0f/255.0f blue:201.0f/255.0f alpha:1.0f];
             dayLabel.text = self.daysOfWeekPrefixes[i];
             dayLabel.textAlignment = NSTextAlignmentCenter;
@@ -64,7 +87,7 @@ static NSUInteger const kDaysInWeek                             = 7;
 - (UIButton *)leftChevronButton {
     if (!_leftChevronButton) {
         _leftChevronButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_leftChevronButton setImage:[UIImage imageNamed:@"left_chevron"] forState:UIControlStateNormal];
+        [_leftChevronButton setImage:[UIImage imageNamed:kLeftChevronImageName] forState:UIControlStateNormal];
     }
     return _leftChevronButton;
 }
@@ -72,7 +95,7 @@ static NSUInteger const kDaysInWeek                             = 7;
 - (UILabel *)monthTitleLabel {
     if (!_monthTitleLabel) {
         _monthTitleLabel = [[UILabel alloc] init];
-        _monthTitleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:17.0f];
+        _monthTitleLabel.font = [UIFont fontWithName:kDefaultFontBold size:17.0f];
         _monthTitleLabel.textColor = [UIColor colorWithRed:87.0f/255.0f green:97.0f/255.0f blue:102.0f/255.0f alpha:1.0f];
         _monthTitleLabel.textAlignment = NSTextAlignmentCenter;
         _monthTitleLabel.text = @"December 2013";
@@ -83,7 +106,7 @@ static NSUInteger const kDaysInWeek                             = 7;
 - (UIButton *)rightChevronButton {
     if (!_rightChevronButton) {
         _rightChevronButton = [[UIButton alloc] init];
-        [_rightChevronButton setImage:[UIImage imageNamed:@"right_chevron"] forState:UIControlStateNormal];
+        [_rightChevronButton setImage:[UIImage imageNamed:kRightChevronImageName] forState:UIControlStateNormal];
     }
     return _rightChevronButton;
 }
@@ -177,33 +200,13 @@ static NSUInteger const kDaysInWeek                             = 7;
     [super updateConstraints];
     
     NSDictionary *viewsDictionary = @{@"headerView": self.headerView,
-                                      @"leftChevronButton": self.leftChevronButton,
-                                      @"monthTitleLabel": self.monthTitleLabel,
-                                      @"rightChevronButton": self.rightChevronButton,
                                       @"collectionView": self.collectionView};
     
-    NSDictionary *metrics = @{@"width": @316,
-                              @"headerViewHeight": @101,
-                              @"collectionViewHeight": @379,
-                              @"dayLabelWidth": @15,
-                              @"dayLabelHeight": @21,
-                              @"chevronWidth": @26,
-                              @"chevronHeight": @27,
-                              @"chevronSpacing": @10,
-                              @"monthTitleLabelHeight": @21};
+    NSDictionary *metrics = @{@"width": @(kViewWidth),
+                              @"headerViewHeight": @(kHeaderViewHeight),
+                              @"collectionViewHeight": @([self collectionViewHeight])};
     
-    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-chevronSpacing-[leftChevronButton(==chevronWidth)]" options:0 metrics:metrics views:viewsDictionary]];
-    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[leftChevronButton(==chevronHeight)]" options:0 metrics:metrics views:viewsDictionary]];
-    [self.leftChevronButton centerInContainerOnAxis:NSLayoutAttributeCenterY];
-    
-    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[rightChevronButton(==chevronWidth)]-chevronSpacing-|" options:0 metrics:metrics views:viewsDictionary]];
-    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[rightChevronButton(==chevronHeight)]" options:0 metrics:metrics views:viewsDictionary]];
-    [self.rightChevronButton centerInContainerOnAxis:NSLayoutAttributeCenterY];
-    
-    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[monthTitleLabel]-0-|" options:0 metrics:metrics views:viewsDictionary]];
-    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[monthTitleLabel(==monthTitleLabelHeight)]" options:0 metrics:metrics views:viewsDictionary]];
-    [self.monthTitleLabel centerInContainerOnAxis:NSLayoutAttributeCenterY];
-    
+    // Add horizontal constraints for main subviews
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[headerView(==width)]-0-|"
                                                                  options:0
                                                                  metrics:metrics
@@ -214,11 +217,45 @@ static NSUInteger const kDaysInWeek                             = 7;
                                                                  metrics:metrics
                                                                    views:viewsDictionary]];
     
+    // Add vertical constraints for main subviews
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[headerView(==headerViewHeight)]-0-[collectionView(==collectionViewHeight)]"
                                                                  options:0
                                                                  metrics:metrics
                                                                    views:viewsDictionary]];
     
+    [self addConstraintsForHeaderView];
+}
+
+- (void)addConstraintsForHeaderView {
+    NSDictionary *viewsDictionary = @{@"headerView": self.headerView,
+                                      @"leftChevronButton": self.leftChevronButton,
+                                      @"monthTitleLabel": self.monthTitleLabel,
+                                      @"rightChevronButton": self.rightChevronButton};
+    
+    NSDictionary *metrics = @{@"headerViewHeight": @(kHeaderViewHeight),
+                              @"dayLabelWidth": @(kDayLabelSize.width),
+                              @"dayLabelHeight": @(kDayLabelSize.height),
+                              @"chevronWidth": @(kChevronSize.width),
+                              @"chevronHeight": @(kChevronSize.height),
+                              @"chevronSpacing": @(kChevronSpacing),
+                              @"monthTitleLabelHeight": @(kMonthTitleLabelHeight)};
+    
+    // Left Chevron Button Location
+    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-chevronSpacing-[leftChevronButton(==chevronWidth)]" options:0 metrics:metrics views:viewsDictionary]];
+    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[leftChevronButton(==chevronHeight)]" options:0 metrics:metrics views:viewsDictionary]];
+    [self.leftChevronButton centerInContainerOnAxis:NSLayoutAttributeCenterY];
+    
+    // Month Title Label Location
+    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[monthTitleLabel]-0-|" options:0 metrics:metrics views:viewsDictionary]];
+    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[monthTitleLabel(==monthTitleLabelHeight)]" options:0 metrics:metrics views:viewsDictionary]];
+    [self.monthTitleLabel centerInContainerOnAxis:NSLayoutAttributeCenterY];
+    
+    // Right Chevron Button Location
+    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[rightChevronButton(==chevronWidth)]-chevronSpacing-|" options:0 metrics:metrics views:viewsDictionary]];
+    [self.headerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[rightChevronButton(==chevronHeight)]" options:0 metrics:metrics views:viewsDictionary]];
+    [self.rightChevronButton centerInContainerOnAxis:NSLayoutAttributeCenterY];
+    
+    // Add constraints for days of week subviews
     [self.headerView spaceViews:self.dayLabels onAxis:UILayoutConstraintAxisHorizontal withSpacing:-4.0f alignmentOptions:0];
     
     for (UILabel *label in self.dayLabels) {
@@ -290,20 +327,27 @@ static NSUInteger const kDaysInWeek                             = 7;
 - (CGSize)collectionView:(UICollectionView *)collectionView
                   layout:(UICollectionViewLayout*)collectionViewLayout
   sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    CGSize mElementSize = CGSizeMake(44, 44);
-    return mElementSize;
+    return kCollectionViewCellSize;
 }
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
-    return 1.0;
+    return kMinimumInteritemSpacing;
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
-    return 1.0;
+    return kMinimumLineSpacing;
 }
 
 // Layout: Set Edges
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    return UIEdgeInsetsMake(1,1,1,1);  // top, left, bottom, right
+    return kSectionInset;  // top, left, bottom, right
+}
+
+#pragma mark - Utility
+
+- (CGFloat)collectionViewHeight {
+    NSInteger numberOfRows = [self collectionView:self.collectionView numberOfItemsInSection:0] / kDaysInWeek;
+    CGFloat rowHeight = [self collectionView:self.collectionView layout:self.calendarFlowLayout sizeForItemAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]].height;
+    return (CGFloat)numberOfRows * rowHeight + kCollectionViewHeightBuffer;
 }
 
 @end
